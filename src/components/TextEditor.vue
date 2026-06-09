bfb2d1e3ed703c815692a19e5be0d27de5a66a42
vcinex <i@vcinex.com>
2026-06-10 02:23:49 +0800

    文件系统优化

--- src/components/TextEditor.vue
+++ src/components/TextEditor.vue
@@ -23,7 +23,7 @@
 
 <script setup lang="ts">
 import { ref, computed, onMounted } from 'vue'
-import { useFileSystem } from '../../../my-rss-worker/useFileSystem'
+import { useFileSystem } from '../composables/useFileSystem'
 
 const { files, readFile, writeFile, deleteFile, getMetadata, updateFileList } = useFileSystem()
 
---
+++ src/services/fs.ts
@@ -0,0 +1,14 @@
+export interface FileMetadata {
+	name: string;
+	path: string;
+	size: number;
+	mtime: number; // 修改时间
+	type: "file" | "directory";
+}
+
+export interface VFSNode extends FileMetadata {
+	content?: string;
+	children?: Map<string, VFSNode>;
+}
+
+export const FS_STORAGE_KEY = "vfs_root";
