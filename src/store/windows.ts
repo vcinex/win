f763e0fbb6027febdcb87705a32e0080fbcdafa8
vcinex <i@vcinex.com>
2026-06-13 01:44:49 +0800

    zindex优化

--- src/store/windows.ts
+++ src/store/windows.ts
@@ -6,6 +6,7 @@ const BASE_Z_INDEX = 100;
 export const useWindowStore = defineStore('windows', {
   state: () => ({
     windows: [] as WindowState[],
+    histories: [] as string[],
     activeWindowId: '',
     nextZIndex: BASE_Z_INDEX
   }),
@@ -16,6 +17,7 @@ export const useWindowStore = defineStore('windows', {
 
   actions: {
     // 内部私有方法：Z-Index 收敛算法，保持层级健康
+    // 暂时先不用
     _normalizeZIndex() {
       // 按照当前 ZIndex 升序排序
       this.windows.sort((a, b) => a.zIndex - b.zIndex);
@@ -26,6 +28,10 @@ export const useWindowStore = defineStore('windows', {
       this.nextZIndex = BASE_Z_INDEX + this.windows.length;
     },
 
+    findWindow(id: string) {
+      return this.windows.find((item) => item.id === id);
+    },
+
     addWindow(windowState: WindowState) {
       windowState.zIndex = this.nextZIndex++;
       this.windows.push(windowState);
@@ -34,23 +40,41 @@ export const useWindowStore = defineStore('windows', {
 
     removeWindow(id: string) {
       this.windows = this.windows.filter((item) => item.id !== id);
-      if (this.activeWindowId === id) {
-        // 寻找最后一个非最小化窗口
-        const lastVisibleWindow = this.windows.filter((item) => !item.minimized).pop();
-        this.activeWindowId = lastVisibleWindow ? lastVisibleWindow.id : '';
+      this.histories = this.histories.filter((historyId) => historyId !== id);
+
+      // 从 histories（历史栈）末尾向前寻找第一个未最小化的窗口激活
+      let index = this.histories.length - 1;
+      for (; index >= 0; index--) {
+        const historyId = this.histories[index];
+        const win = this.findWindow(historyId);
+        if (!win) {
+          this.histories.splice(index, 1);
+        } else {
+          if (!win.minimized) {
+            this.activeWindowId = win.id;
+            break;
+          }
+        }
       }
-      this._normalizeZIndex(); // 关窗口时顺便清理一下层级碎片
+      if (index === -1) {
+        this.activeWindowId = '';
+      }
     },
 
     focusWindow(id: string) {
       const win = this.windows.find((item) => item.id === id);
       if (!win) return;
 
-      // 只有当它不是最高层时，才提升它，减少不必要的重排
+      // 1. Z-Index 逻辑：只有当它不是最高层时，才提升它，减少不必要的重排
       if (win.zIndex < this.nextZIndex - 1) {
         win.zIndex = this.nextZIndex++;
-        this._normalizeZIndex(); // 提升后进行归一化
       }
+
+      const historyIndex = this.histories.findIndex((historyId) => historyId === id);
+      if (historyIndex !== -1) {
+        this.histories.splice(historyIndex, 1);
+      }
+      this.histories.push(win.id);
 
       this.activeWindowId = id;
       win.minimized = false;
